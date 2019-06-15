82c0dbf8b2713e7ff2cf67d9fef4383b4e5bfda2
Flávia Rosado Lima <flaviarosadolima@hotmail.com>
2019-06-15 20:33:39 -0300

    Criação API e indicadores iniciais

---
+++ FishTech/FishTechAPI/Controllers/IndicadorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FishTechWebManager._Repository.Core;
+using FishTechWebManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FishTechAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndicadorController : ControllerBase
+    {
+        private readonly IIndicadorRepository _IREP;
+
+        public IndicadorController(IIndicadorRepository IREP)
+        {
+            _IREP = IREP;
+        }
+
+        [HttpGet("Id")]
+        public Indicador Get(int Id)
+        {
+            return _IREP.GetbyID(Id);
+        }
+    }
+}
