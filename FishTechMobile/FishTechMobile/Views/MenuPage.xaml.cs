b82a7a013f7ad582f590138eea25d7381d86c366
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2019-06-15 02:15:07 -0300

    Integrando Projeto que Foi iniciado no Computador da Flavia

--- FishTechMobile/FishTechMobile/Views/MenuPage.xaml.cs
+++ FishTechMobile/FishTechMobile/Views/MenuPage.xaml.cs
@@ -15,22 +15,22 @@ namespace FishTechMobile.Views
         public MenuPage()
         {
             InitializeComponent();
-            MasterPage.ListView.ItemSelected += ListView_ItemSelected;
+            //MasterPage.ListView.ItemSelected += ListView_ItemSelected;
         }
 
         private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            var item = e.SelectedItem as MenuPageMenuItem;
-            if (item == null)
-                return;
+            //var item = e.SelectedItem as MenuPageMenuItem;
+            //if (item == null)
+            //    return;
 
-            var page = (Page)Activator.CreateInstance(item.TargetType);
-            page.Title = item.Title;
+            //var page = (Page)Activator.CreateInstance(item.TargetType);
+            //page.Title = item.Title;
 
-            Detail = new NavigationPage(page);
-            IsPresented = false;
+            //Detail = new NavigationPage(page);
+            //IsPresented = false;
 
-            MasterPage.ListView.SelectedItem = null;
+            //MasterPage.ListView.SelectedItem = null;
         }
     }
 }
