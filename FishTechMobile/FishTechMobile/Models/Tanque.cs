83dd26e8d68b9744e3e19a083094e427218398da
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2019-06-16 09:51:27 -0300

    Alguns Detalhes

--- FishTechMobile/FishTechMobile/Models/Tanque.cs
+++ FishTechMobile/FishTechMobile/Models/Tanque.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace FishTechMobile.Models
@@ -14,5 +15,47 @@ namespace FishTechMobile.Models
         public Double Biomassa { get; set; }
         public String DescricaoEspecie { get => EspeciePeixe.Descricao; }
 
+        public string OD { get; set; }
+        public float Temp { get; set; }
+        public float Ph { get; set; }
+        public float Turbidez { get; set; }
+
+        public Tanque()
+        {
+            
+            OD = RandomNumber.Between(10, 30).ToString() + "mg/l";
+           
+            Temp = RandomNumber.Between(27, 39);
+          
+            Ph = RandomNumber.Between(5, 14);
+          
+            Turbidez = RandomNumber.Between(1, 5);
+        }
+    }
+
+    public static class RandomNumber
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        public static int Between(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // We are using Math.Max, and substracting 0.00000000001, 
+            // to ensure "multiplier" will always be between 0.0 and .99999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+            // We need to add one to the range, to allow for the rounding done with Math.Floor
+            int range = maximumValue - minimumValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimumValue + randomValueInRange);
+        }
     }
 }
--- FishTechMobile/FishTechMobile/ViewModels/TanqueDetailViewModel.cs
+++ FishTechMobile/FishTechMobile/ViewModels/TanqueDetailViewModel.cs
@@ -43,43 +43,43 @@ namespace FishTechMobile.ViewModels
                 {
                     new Entry(6.5f)
                     {
-                        Label = "10/06/2019",
+                        Label = "09/06/2019",
                         ValueLabel = "6.5",
                         Color = SKColor.Parse("#FFA000")
                     },
                      new Entry(6.8f)
                     {
-                        Label = "11/06/2019",
+                        Label = "10/06/2019",
                         ValueLabel = "6.8",
                         Color = SKColor.Parse("#D32F2F")
                     },
                       new Entry(5.9f)
                     {
-                        Label = "12/06/2019",
+                        Label = "11/06/2019",
                         ValueLabel = "5.9",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                        new Entry(4.7f)
                     {
-                        Label = "13/06/2019",
+                        Label = "12/06/2019",
                         ValueLabel = "4.7",
                         Color = SKColor.Parse("#CDDC39")
                     },
                         new Entry(5.8f)
                     {
-                        Label = "14/06/2019",
+                        Label = "13/06/2019",
                         ValueLabel = "5.8",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                          new Entry(7.4f)
                     {
-                        Label = "15/06/2019",
+                        Label = "14/06/2019",
                         ValueLabel = "7.4",
                         Color = SKColor.Parse("#D32F2F")
                     },
                           new Entry(7.4f)
                     {
-                        Label = "16/06/2019",
+                        Label = "15/06/2019",
                         ValueLabel = "7.4",
                         Color = SKColor.Parse("#D32F2F")
                     },
@@ -97,43 +97,43 @@ namespace FishTechMobile.ViewModels
                 {
                     new Entry(36.1f)
                     {
-                        Label = "10/06/2019",
+                        Label = "09/06/2019",
                         ValueLabel = "36.1",
                         Color = SKColor.Parse("#FFA000")
                     },
                      new Entry(37.1f)
                     {
-                        Label = "11/06/2019",
+                        Label = "10/06/2019",
                         ValueLabel = "37.1",
                         Color = SKColor.Parse("#D32F2F")
                     },
                       new Entry(33.1f)
                     {
-                        Label = "12/06/2019",
+                        Label = "11/06/2019",
                         ValueLabel = "33.1",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                        new Entry(31.1f)
                     {
-                        Label = "13/06/2019",
+                        Label = "12/06/2019",
                         ValueLabel = "31.1",
                         Color = SKColor.Parse("#CDDC39")
                     },
                         new Entry(36.1f)
                     {
-                        Label = "14/06/2019",
+                        Label = "13/06/2019",
                         ValueLabel = "36.1",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                          new Entry(39.1f)
                     {
-                        Label = "15/06/2019",
+                        Label = "14/06/2019",
                         ValueLabel = "39.1",
                         Color = SKColor.Parse("#D32F2F")
                     },
                           new Entry(36.1f)
                     {
-                        Label = "16/06/2019",
+                        Label = "15/06/2019",
                         ValueLabel = "36.1",
                         Color = SKColor.Parse("#FFA000")
                     },
@@ -151,43 +151,43 @@ namespace FishTechMobile.ViewModels
                 {
                     new Entry(33.1f)
                     {
-                        Label = "10/06/2019",
+                        Label = "09/06/2019",
                         ValueLabel = "33.1",
                         Color = SKColor.Parse("#CDDC39")
                     },
                      new Entry(34.1f)
                     {
-                        Label = "11/06/2019",
+                        Label = "10/06/2019",
                         ValueLabel = "34.1",
                         Color = SKColor.Parse("#CDDC39")
                     },
                       new Entry(34.8f)
                     {
-                        Label = "12/06/2019",
+                        Label = "11/06/2019",
                         ValueLabel = "34.1",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                        new Entry(34.8f)
                     {
-                        Label = "13/06/2019",
+                        Label = "12/06/2019",
                         ValueLabel = "34.8",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                         new Entry(35.6f)
                     {
-                        Label = "14/06/2019",
+                        Label = "13/06/2019",
                         ValueLabel = "35.6",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                          new Entry(36.2f)
                     {
-                        Label = "15/06/2019",
+                        Label = "14/06/2019",
                         ValueLabel = "36.2",
                         Color = SKColor.Parse("#D32F2F")
                     },
                           new Entry(36.8f)
                     {
-                        Label = "16/06/2019",
+                        Label = "15/06/2019",
                         ValueLabel = "36.8",
                         Color = SKColor.Parse("#D32F2F")
                     },
@@ -205,43 +205,43 @@ namespace FishTechMobile.ViewModels
                 {
                     new Entry(33.1f)
                     {
-                        Label = "10/06/2019",
+                        Label = "09/06/2019",
                         ValueLabel = "33.1",
                         Color = SKColor.Parse("#CDDC39")
                     },
                      new Entry(34.1f)
                     {
-                        Label = "11/06/2019",
+                        Label = "10/06/2019",
                         ValueLabel = "34.1",
                         Color = SKColor.Parse("#CDDC39")
                     },
                       new Entry(34.8f)
                     {
-                        Label = "12/06/2019",
+                        Label = "11/06/2019",
                         ValueLabel = "34.1",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                        new Entry(34.8f)
                     {
-                        Label = "13/06/2019",
+                        Label = "12/06/2019",
                         ValueLabel = "34.8",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                         new Entry(35.6f)
                     {
-                        Label = "14/06/2019",
+                        Label = "13/06/2019",
                         ValueLabel = "35.6",
                         Color = SKColor.Parse("#FFEB3B")
                     },
                          new Entry(36.2f)
                     {
-                        Label = "15/06/2019",
+                        Label = "14/06/2019",
                         ValueLabel = "36.2",
                         Color = SKColor.Parse("#D32F2F")
                     },
                           new Entry(36.8f)
                     {
-                        Label = "16/06/2019",
+                        Label = "15/06/2019",
                         ValueLabel = "36.8",
                         Color = SKColor.Parse("#D32F2F")
                     },
